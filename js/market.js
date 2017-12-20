ae32c8f005ab5a4fb47839c960532b36efa5aa2e
许鑫君 <1205730728@qq.com>
2017-12-20 14:40:39 +0800

    xuxinjun--market.w

--- js/market.js
+++ js/market.js
@@ -20,6 +20,72 @@ define(function(require){
 				}
 			});
 		},
+		//获得交易记录--许鑫君
+		getTransactionRecord:function(){
+			$.ajax({
+				url:"",
+				async:false,
+				dataType:"json",
+				type:"GET",
+				success:function(){
+					
+				},
+				error:function(){
+					
+				}
+			});
+		},
+		//验证卖出的产品id是否存在--许鑫君
+		checkProductionId:function(productionCode){
+			var is_live;
+			$.ajax({
+				url:"",
+				async:false,
+				dataType:"json",
+				type:"GET",
+				success:function(data){
+					is_live=true;
+				},
+				error:function(){
+					is_live=false;
+				}
+			});
+			return is_live;
+		},
+		//出售商品--许鑫君
+		sellProduction:function(productionCode){
+			var is_success;
+			$.ajax({
+				url:"",
+				async:false,
+				dataType:"json",
+				type:"GET",
+				success:function(data){
+					is_success=true;
+				},
+				error:function(){
+					is_success=false;
+				}
+			});
+			return is_success;
+		},
+		//下架--许鑫君
+		notSold:function(id){
+			var is_success;
+			$.ajax({
+				url:"",
+				async:false,
+				dataType:"json",
+				type:"GET",
+				success:function(data){
+					is_success=true;
+				},
+				error:function(){
+					is_success=false;
+				}
+			});
+			return is_success;
+		},
 	};
 
 });
