041e881affc9e46716541d28dcef0441007b04ec
buxcxfly <buxcxfly@gmail.com>
2017-12-20 11:40:10 +0800

    错误

--- recharge.js
+++ recharge.js
@@ -1,11 +1,11 @@
 define(function(require){
 	var $ = require("jquery");
 	var justep = require("$UI/system/lib/justep");
-  var Message = require("$UI/system/components/justep/common/common");
+//	var Message = require("$UI/system/components/justep/common/common");
 
 	var Model = function(){
 		this.callParent();
-    this.flag = Message.flag;
+//		this.flag = Message.flag;
 	};
 
 	Model.prototype.setupButtonClick = function(event){
