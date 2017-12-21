e8fc32d1c2ae6ee5d475aa13897e092d642d3b08
buxcxfly <buxcxfly@gmail.com>
2017-12-21 14:26:22 +0800

    site

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 define(function(require){
 	var $ =require('jquery');
 	return{
-		site: "http://site",
+		site: "http://47.96.154.21",
 		eggval : function () {
 			$.ajax({
 				url:"http://127.0.0.1:8081/api/v1/config/eggval",
