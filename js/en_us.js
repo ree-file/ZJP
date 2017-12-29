02e92fb364b5dc593d084179e289c72a3f042d00
buxcxfly <buxcxfly@gmail.com>
2017-12-29 23:21:55 +0800

    xcx

---
+++ js/en_us.js
@@ -0,0 +1,12 @@
+define(function(require){
+
+	return {
+		showprompt:['please login again','E-mail can not be empty','password can not be blank',"E-mail or password is wrong","E-mail or password can not be empty"],
+		bankAddind:['Bank card added','Please bind the card holder\'s card','Cardholder','Card Number','Bank','Submit and review'],
+		bankBanding:['Bank card binding','Add a bank card','Delete'],
+		community:["Family","Family Structure","Family Welfare","Family Number","Direct Acceleration","Family Acceleration","A Family","B Family","C Family","Total","Children","Grandchildren"],
+		illustrate:["Product Manual","Product Description","Product Rules"],
+		mailLogin:["Log in","E-mail","password","Please input the email address","forget password"],
+		market:["Market","Market Place","Transaction Record","Quick Sell","Comprehensive Sorting","Filter","Remaining","Number of Children","Number of Grandchildren","Buy","Loading Completed","Product Number","Amount","status","Transaction Hour","Cat Nest ID","Price（$）"]
+	}
+});
